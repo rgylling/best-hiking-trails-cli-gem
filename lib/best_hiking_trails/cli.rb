5f49495dcc9afe35291cee3914576ef9e0206500
rgylling <mikegylling@yahoo.com>
2016-11-09 16:39:14 -0800

    building out temp CLI interface

--- lib/best_hiking_trails/cli.rb
+++ lib/best_hiking_trails/cli.rb
@@ -2,13 +2,13 @@
 class BestHikingTrails::CLI
 
   def call
-    puts "The top 25 hiking trails"
     list_trails
     trail_info
   end
 
   def list_trails
     #get hiking trails ******FAKE INFO FOR NOW*****
+    puts "The top 25 hiking trails"
     puts <<-DOC.gsub /^\s*/, ''
     1. Inca Trail
     2. Appalachian Trail
@@ -18,7 +18,19 @@ class BestHikingTrails::CLI
   end
 
   def trail_info
-    puts "Enter the number of the trail you would like more information on."
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the trail you would like more information on, list to relist the trails, or type exit to exit the program."
+      input = gets.strip
+      case input
+        when "1"
+          puts "More info on trail 1"
+        when "2"
+          puts "More info on trail 2"
+        when "list"
+        list_trails  
+      end
+    end
   end
 
 end
