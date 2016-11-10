46e73e74782bc4afa5889ce38df6b64dc75353de
rgylling <mikegylling@yahoo.com>
2016-11-10 12:59:12 -0800

    figured out how to get a list of every trail

--- lib/best_hiking_trails.rb
+++ lib/best_hiking_trails.rb
@@ -1,4 +1,6 @@
+require 'open-uri'
 require 'nokogiri'
+require 'pry'
 require_relative "./best_hiking_trails/version"
 require_relative "./best_hiking_trails/trails"
 require_relative "./best_hiking_trails/cli"
--- lib/best_hiking_trails/trails.rb
+++ lib/best_hiking_trails/trails.rb
@@ -9,9 +9,20 @@ class BestHikingTrails::Trail
 
   def self.scrape_trails
     trails = []
+    deals << self.scrape_best_trails
     #go to 25 best hiking trails, extract the properties instantiate a trail
 
     trails
+
+  end
+
+  def self.scrape_best_trails
+    
+    doc = Nokogiri::HTML(open("https://www.theoutbound.com/theoutbound/the-best-25-hikes-in-america"))
+    testing = doc.xpath("//h3").each_with_index do |name, i|
+      puts name.content
+      break if i == 24;
+    end
 
   end
 
