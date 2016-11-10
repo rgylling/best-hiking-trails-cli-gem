65d99faa8d1ad60eb204db80ed5912bf47916bd9
rgylling <mikegylling@yahoo.com>
2016-11-10 13:21:57 -0800

    commented out how the user will recieve more information about a hike

--- lib/best_hiking_trails/trails.rb
+++ lib/best_hiking_trails/trails.rb
@@ -19,11 +19,17 @@ class BestHikingTrails::Trail
   def self.scrape_best_trails
     trail_names = []
     doc = Nokogiri::HTML(open("https://www.theoutbound.com/theoutbound/the-best-25-hikes-in-america"))
-    testing = doc.xpath("//h3").each_with_index do |name, i|
+    trails = doc.xpath("//h3").each_with_index do |name, i|
       trail_names << name.content
       break if i == 24;
     end
     trail_names
   end
+
+  #test to see how I will get more information on a specific hike
+  # doc2 = Nokogiri::HTML(open("https://www.theoutbound.com/colorado/hiking/hike-to-hanging-lake"))
+  # title = doc2.search(".dynamic-height-container .adventure-description p").text
+  # end
+
 
 end
